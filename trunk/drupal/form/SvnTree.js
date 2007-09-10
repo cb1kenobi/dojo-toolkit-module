f56bfd6c591b14846f5913cc3f0069a2a8da2a1f
chris <chris@fdca2fe5-a254-0410-b265-8553752079d8>
2007-09-10 01:49:23 +0000

    Updated the SvnTree to work with Dojo 0.9.

--- trunk/drupal/form/SvnTree.js
+++ trunk/drupal/form/SvnTree.js
@@ -10,15 +10,9 @@ dojo.declare(
 		_tree: null,
 
 		postCreate: function() {
-			/*
-			var controller = dojo.widget.createWidget("TreeLoadingControllerV3", { RpcUrl: "/dojo/svn" });
-			var docIcons = dojo.widget.createWidget("TreeDocIconExtension");
-			var selector = dojo.widget.createWidget("TreeSelectorV3");
-			var emphasize = dojo.widget.createWidget("TreeEmphasizeOnSelect", { selector: selector.widgetId });
-
-			dojo.event.connect("around", controller, "loadProcessResponse", this, "_onExpandResponse");
-			dojo.event.topic.subscribe(selector.eventNames.select, this, "_onNodeSelect");
-			*/
+			var self = this;
+			var opened = "dojoSvnTreeFolderOpened";
+			var closed = "dojoSvnTreeFolderClosed";
 
 			var d = document.createElement("div");
 			dojo.place(d, this.domNode, "after");
@@ -27,66 +21,34 @@ dojo.declare(
 				query: { type: "dir" },
 				labelAttr: "name",
 				typeAttr: "type",
-				store: new drupal.data.SvnStore({ url: "/dojo/svn" })
+				store: new drupal.data.SvnStore({ url: "/dojo/svn" }),
+				getIconClass: function() { return closed; }
 			}, d);
 
-			// { listeners: [controller.widgetId, docIcons.widgetId, selector.widgetId] });
+			dojo.connect(this._tree, "addChild", this, "_createToolTip");
+
+			dojo.subscribe(this._tree.id, null, function(e) {
+				var n = e.node;
+				if (/^toggleOpen$|^zoomIn$|^zoomOut$/i.test(e.event)) {
+					var b = (n.isExpanded || /loading/i.test(n.state));
+					dojo.removeClass(n.iconNode, b ? closed : opened);
+					dojo.addClass(n.iconNode, b ? opened : closed);
+				} else if (e.event == "execute") {
+					self.domNode.value = n.item.id;
+				}
+			});
 
 			dojo.addClass(this._tree.domNode, "dojoSvnTree");
-			/*
-			dojo.event.topic.subscribe(this._tree.eventNames.afterAddChild, this, "_onAddNode");
-			*/
-			//this._tree.setChildren(this._buildNodes(this.data));
 		},
 
-/*
-		_buildNodes: function(data, parentObjectId) {
-			if (!parentObjectId) {
-				parentObjectId = "";
-			}
-			var nodes = [];
-			for (var i = 0; i < data.length; i++) {
-				nodes.push({
-					item: {
-						label: data[i].name,
-						isFolder: true,
-						objectId: parentObjectId + "/" + data[i].name,
-						revision: data[i].revision,
-						author: data[i].author,
-						date: data[i].date
-					}
-				});
-			}
-			return nodes;
-		},
-*/
- 		/*
-		_onExpandResponse: function(invocation) {
-			invocation.args[1] = this._buildNodes(invocation.args[1], invocation.args[0].objectId);
-			invocation.proceed();
-		},
-		*/
-
-		_onNodeSelect: function(msg) {
-			this.domNode.value = msg.node.objectId;
-		},
-
-		_onAddNode: function(msg) {
-			var node = msg.child;
-
-			var str = "<div><strong>" + node.objectId + "</strong></div>";
-			str += "<div>Revision: " + node.revision + "</div>";
-			str += "<div>Author: " + node.author + "</div>";
-			str += "<div>Date: " + node.date + "</div>";
-
-			var id = "TreeNode" + Math.round(Math.random()*10000000);
-			node.domNode.id = id;
-
+		_createToolTip: function(node) {
 			var d = document.createElement("div");
-			d.innerHTML = str;
+			d.innerHTML = "<div><strong>" + node.label + "</strong></div><div>Revision: " + node.item.revision + "</div><div>Author: " + node.item.author + "</div><div>Date: " + node.item.date + "</div>";
 			dojo.style(d, 'display', 'none');
-			dojo.place(d, this.domNode, "after");
-			var t = new dijit.Tooltip({ connectId: id }, d);
+			dojo.place(d, self.domNode, "after");
+			node.labelNode.id = "node" + Math.round(Math.random() * 10000000);
+			var t = new dijit.Tooltip({ connectId: node.labelNode.id }, d);
+			dojo.connect(node, "addChild", this, "_createToolTip");
 		}
 	}
 );
