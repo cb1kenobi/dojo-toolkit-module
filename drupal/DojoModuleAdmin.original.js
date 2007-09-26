b584752ff925bb5db87b3320d7f522a58f41b364
Chris Barber <chris@cb1inc.com>
2007-09-26 01:13:28 +0000

    Deleted unneed original copies since I found a way to do builds without disturbing the Drupal directory.

--- drupal/DojoModuleAdmin.original.js
+++
@@ -1,7 +0,0 @@
-dojo.provide("drupal.DojoModuleAdmin");
-
-dojo.require("drupal.PleaseWait");
-dojo.require("drupal.data.SvnStore");
-dojo.require("drupal.form.CustomizeToolbar");
-dojo.require("drupal.form.GoogleMapKeyTable");
-dojo.require("drupal.form.SvnTree");
