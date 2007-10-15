f9405a91df30374579e3b182104d4ca1791a439e
chris <chris@fdca2fe5-a254-0410-b265-8553752079d8>
2007-10-15 05:41:10 +0000

    Whoops, didn't mean to tag 2.0 yet.

--- tags/release-2.0/drupal/DojoModuleAdminDeps.js
+++
@@ -1,8 +0,0 @@
-dojo.provide("drupal.DojoModuleAdminDeps");
-
-dojo.require("dojo.data.ItemFileWriteStore");
-dojo.require("dojox.collections.ArrayList");
-dojo.require("dijit.form._FormWidget");
-dojo.require("dijit._Templated");
-dojo.require("dijit.Tree");
-dojo.require("dijit.Tooltip");
