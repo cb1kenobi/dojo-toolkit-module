e1f37762dc94b8e41cb097c305712f7cb23550dc
chris <chris@fdca2fe5-a254-0410-b265-8553752079d8>
2007-11-02 20:24:09 +0000

    Fixed the SVN repository tree to work with Dojo 1.0.

--- trunk/drupal/data/SvnStore.js
+++ trunk/drupal/data/SvnStore.js
@@ -5,68 +5,40 @@ dojo.declare(
 	"drupal.data.SvnStore",
 	dojo.data.ItemFileWriteStore,
 	{
-		isItemLoaded: function(item) {
-			if (this.getValue(item, "type") === "stub") {
-				return false;
-			}
-			return true;
-		},
+		getValues: function(/* item */ parentItem, 
+							/* attribute-name-string */ attribute){
+			//	summary: 
+			//		See dojo.data.api.Read.getValues()
+	
+			this._assertIsItem(parentItem);
+			this._assertIsAttribute(attribute);
 
-		loadItem: function(keywordArgs){
-			var item = keywordArgs.item;
-			this._assertIsItem(item);
-
-			var path = this.getValue(item, "path");
-			var self = this;
-
-			var d = dojo.xhrGet({
-				url: this._jsonFileUrl + path,
-				handleAs: "json-comment-optional"
-			});
-
-			d.addCallback(function(data){
-				self.fetchItemByIdentity({
-					identity: path,
-					onItem: function(parentItem) {
-						dojo.forEach(self.getValues(parentItem, "children"), function(item) { self.deleteItem(item); });
-						self.unsetAttribute(parentItem, "children");
-
-						for (var i = 0; i < data.length; i++) {
-							var item = self.newItem(data[i], { parent: parentItem, attribute: "children" });
-							var attributes = self.getAttributes(item);
-							for (var a in attributes) {
-								var values = self.getValues(item, attributes[a]);
-								for (var j = 0; j < values.length; j++) {
-									var stub = values[j];
-									if (typeof stub === "object" && stub["type"] == "stub") {
-										for (var k in stub) {
-											if (!dojo.isArray(stub[k])) {
-												stub[k] = [stub[k]];
-											}
-										}
-										self._arrayOfAllItems.push(stub);
-										stub[self._storeRefPropName] = self;
-										stub[self._itemNumPropName] = self._arrayOfAllItems.length - 1;
-										values[j] = stub;
-									}
-								}
-							}
-						}
-
-						if (keywordArgs.onItem) {
-							var scope = keywordArgs.scope ? keywordArgs.scope : dojo.global;
-							keywordArgs.onItem.call(scope, null);
+			if (attribute == "children" && parentItem.id) {
+				var self = this;
+				var d = dojo.xhrGet({
+					url: this._jsonFileUrl + parentItem.id,
+					sync: true,
+					handleAs: "json",
+					load: function(data, ioArgs) {
+						for (var i=0; i<data.length; i++) {
+							data[i]["name"] = [ data[i]["name"] ];
+							self._arrayOfAllItems.push(data[i]);
+							data[i][self._storeRefPropName] = self;
+							data[i][self._itemNumPropName] = self._arrayOfAllItems.length - 1;
+							parentItem["children"].push(data[i]);
 						}
 					}
 				});
-			});
+			}
 
-			d.addErrback(function(error) {
-				if (keywordArgs.onError) {
-					var scope = keywordArgs.scope ? keywordArgs.scope : dojo.global;
-					keywordArgs.onError.call(scope, error);
-				}
-			});
+			return parentItem[attribute] || []; // Array
+		},
+
+		hasAttribute: function(item, attr) {
+			if (attr == "children") {
+				return true;
+			}
+			return this.getValues(item, attribute).length > 0;
 		}
 	}
 );
