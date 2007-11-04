fa14b159f0614c7e30d22066541fa4fa386d9363
Chris Barber <chris@cb1inc.com>
2007-11-04 02:41:06 +0000

    Finished upgrade of SVN feature to Dojo 1.0.  Updated change log.  Fixed loading of Dojo on all pages.

--- drupal/form/SvnTree.js
+++ drupal/form/SvnTree.js
@@ -10,8 +10,6 @@ dojo.declare(
 
 		postCreate: function() {
 			var self = this;
-			var opened = "dojoSvnTreeFolderOpened";
-			var closed = "dojoSvnTreeFolderClosed";
 
 			var d = document.createElement("div");
 			dojo.place(d, this.domNode, "after");
@@ -22,33 +20,25 @@ dojo.declare(
 				typeAttr: "type",
 				store: new drupal.data.SvnStore({ url: "/dojo/svn" }),
 				persist: false,
-				getIconClass: function() { return closed; }
+				getIconClass: function() { return "dojoSvnTreeFolderClosed"; }
 			}, d);
 
-			dojo.connect(this._tree, "addChild", this, "_createToolTip");
-
-			dojo.subscribe(this._tree.id, null, function(e) {
-				var n = e.node;
-				if (/^toggleOpen$|^zoomIn$|^zoomOut$/i.test(e.event)) {
-					var b = (n.isExpanded || /loading/i.test(n.state));
-					dojo.removeClass(n.iconNode, b ? closed : opened);
-					dojo.addClass(n.iconNode, b ? opened : closed);
-				} else if (e.event == "execute") {
-					self.domNode.value = n.item.id;
-				}
-			});
-
+			this._setupNode(this._tree);
+			
 			dojo.addClass(this._tree.domNode, "dojoSvnTree");
 		},
 
 		_createToolTip: function(node) {
 			var d = document.createElement("div");
 			d.innerHTML = "<div><strong>" + node.label + "</strong></div><div>Revision: " + node.item.revision + "</div><div>Author: " + node.item.author + "</div><div>Date: " + node.item.date + "</div>";
-			dojo.style(d, 'display', 'none');
-			dojo.place(d, self.domNode, "after");
-			node.labelNode.id = "node" + Math.round(Math.random() * 10000000);
-			var t = new dijit.Tooltip({ connectId: node.labelNode.id }, d);
+			dojo.body().appendChild(d);		
+			var t = new dijit.Tooltip({ connectId: [node.iconNode,	node.labelNode] }, d);
+			this._setupNode(node);
+		},
+
+		_setupNode: function(node) {
 			dojo.connect(node, "addChild", this, "_createToolTip");
+			dojo.connect(node, "_setExpando", function() { node.iconNode.className = "dijitInline dijitTreeIcon dojoSvnTreeFolder" + (node.isExpanded ? "Opened" : "Closed"); });
 		}
 	}
 );
