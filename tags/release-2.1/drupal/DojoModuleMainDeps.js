9c1048c292f28ac9136bb45bedfe36b47161d894
chris <chris@fdca2fe5-a254-0410-b265-8553752079d8>
2007-11-09 06:47:33 +0000

    Not quite ready to release.

--- tags/release-2.1/drupal/DojoModuleMainDeps.js
+++
@@ -1,8 +0,0 @@
-dojo.provide("drupal.DojoModuleMainDeps");
-
-dojo.require("dojo.fx");
-dojo.require("dijit._Widget");
-dojo.require("dijit.Editor");
-dojo.require("dijit._editor.plugins.AlwaysShowToolbar");
-dojo.require("dijit._editor.plugins.EnterKeyHandling");
-dojo.require("dijit._editor.plugins.LinkDialog");
